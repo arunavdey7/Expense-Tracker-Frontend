f6bc2f21343219939552f7b84f76d72e6d43a22a
Arunav Dey <arunav.dey.7@gmail.com>
2022-06-05 13:20:03 +0530

    remove creds

--- src/utils/Expenses.js
+++ src/utils/Expenses.js
@@ -1,27 +1,3 @@
-export const getMonthlyExpenditure = async (date) => {
-    var requestOptions = {
-        method: 'GET',
-        mode:'cors',
-        headers:{
-            'Content-type':'Application/json'
-        }
-      }
-    const response = await fetch(`http://ec2-54-196-230-223.compute-1.amazonaws.com:8000?date=${date}`, requestOptions)
-    const {
-        success,
-        total_expenditure
-    } = await response.json()
-    if(success)
-    {
-        return {
-            success,
-            total_expenditure
-        }
-    }
-    return {
-        success
-    }
-}
 export const getMonthlyTransactions = async (date) => {
     var requestOptions = {
         method: 'GET',
@@ -30,7 +6,7 @@ export const getMonthlyTransactions = async (date) => {
             'Content-type':'Application/json'
         }
       }
-    const response = await fetch(`http://ec2-54-196-230-223.compute-1.amazonaws.com:8000/expenses?date=${date}`, requestOptions)
+    const response = await fetch(`/expenses?date=${date}`, requestOptions)
     const {
         success,
         transactions,
